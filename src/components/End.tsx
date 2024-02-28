785bb3380e4a82819ae7f6cf4b4aa411ee5a3b56
cccrespo19 <140754478+cccrespo19@users.noreply.github.com>
2024-02-28 13:01:46 -0600

    add random text emojis in end page

--- src/components/End.tsx
+++ src/components/End.tsx
@@ -1,11 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { user } from '../App';
+import { textEmojis } from '../lib/textEmojis';
 
 type EndProps = {
 	setUser: React.Dispatch<React.SetStateAction<user>>;
 };
 
 function End({ setUser }: EndProps) {
+	const [emoji, setEmoji] = useState(() => {
+		const emojiIndex = Math.floor(Math.random() * (textEmojis.length - 1));
+		return textEmojis[emojiIndex]
+	});
+
 	// FIXME: currently runs twice bc of strict mode so sessionIndex goes up twice instead of once
 	useEffect(() => {
 		const currentDate = new Date();
@@ -21,8 +27,7 @@ function End({ setUser }: EndProps) {
 		<main className="flex-1 px-4 md:w-96 md:self-center">
 			<h1 className="py-8 text-center text-3xl">Congratulations!</h1>
 			<p className="py-1 text-lg">Good job! See you in a couple days</p>
-			{/* TODO: random text emoji */}
-			<p className="py-1 text-lg">(˵¯͒〰¯͒˵)</p>
+			<p className="py-1 text-lg">{emoji}</p>
 			{/* TODO: random quotes */}
 			<p className="py-1 text-lg">Inspirational quote 2</p>
 		</main>
